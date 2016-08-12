b3667b08e720395b1ff934639d7ae7f3f4367996
Benton Drew <benton.s.drew@drewantech.com>
2016-08-12 16:25:16 -0700

    Implement web page user input to provide matrix parameters.

--- generate_random_matrix_web_server.py
+++ generate_random_matrix_web_server.py
@@ -4,14 +4,34 @@
 
 from flask import (Flask,
                    render_template,
-                   request)
+                   request,
+                   flash)
 from os import urandom
 
 app = Flask(__name__)
 app.secret_key = urandom(32)
 
 
-@app.route('/', methods=['GET'])
+@app.route('/', methods=['GET', 'POST'])
 def index():
   if request.method == 'GET':
     return render_template('index.html')
+  if request.method == 'POST':
+    try:
+      try:
+        int(request.form('matrix_rows'))
+        int(request.form('matrix_columns'))
+      except Exception as e:
+        raise e
+      try:
+        float(request.form('max_random_value'))
+      except Exception as e:
+        raise e
+      flash('Matrix parameters: Rows = {}, Columns = {}, Max random value = {}'
+            .format(request.form('matrix_rows'),
+                    request.form('matrix_columns'),
+                    request.form('max_random_value')))
+      return render_template('index.html')
+    except Exception as e:
+      flash(e)
+      return render_template('index.html')
