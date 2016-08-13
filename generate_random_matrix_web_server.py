a2c1f8897f6213aecaf17ff177587bc47cfcbfc3
Benton Drew <benton.s.drew@drewantech.com>
2016-08-12 18:30:57 -0700

    Revert "Make input feedback error flashed to user more understandable."
    This reverts commit 19eb4f50333cf019b16a7959f53da510d0ce0883.

--- generate_random_matrix_web_server.py
+++ generate_random_matrix_web_server.py
@@ -22,26 +22,18 @@ def index():
         int(request.form['matrix_rows'])
         int(request.form['matrix_columns'])
       except Exception as e:
-        raise RuntimeError('Please provide integer values '
-                           'for the matrix rows and columns.')
-      try:
-        int(request.form['number_matrices'])
-      except Exception as e:
-        raise RuntimeError('Please provide integer values '
-                           'for the number of matrices to be generated.')
+        raise e
       try:
         float(request.form['max_random_value'])
       except Exception as e:
         raise RuntimeError('Please provide a number '
                            'for the max random value.')
       flash('Matrix parameters: Rows = {}, '
-                               'Columns = {}, '
-                               'Max random value = {}, '
-                               'Number to be create = {}'
+            'Columns = {}, '
+            'Max random value = {}'
             .format(request.form['matrix_rows'],
                     request.form['matrix_columns'],
-                    request.form['max_random_value'],
-                    request.form['number_matrices']))
+                    request.form['max_random_value']))
       return render_template('index.html')
     except Exception as e:
       flash(e)
