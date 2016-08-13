640fda791a6ea9d2772461d503768c5413220036
Benton Drew <benton.s.drew@drewantech.com>
2016-08-12 17:07:10 -0700

    Add field to indicate the number of matrices to be created.

--- generate_random_matrix_web_server.py
+++ generate_random_matrix_web_server.py
@@ -25,14 +25,23 @@ def index():
         raise RuntimeError('Please provide integer values '
                            'for the matrix rows and columns.')
       try:
+        int(request.form['number_matrices'])
+      except Exception as e:
+        raise RuntimeError('Please provide integer values '
+                           'for the number of matrices to be generated.')
+      try:
         float(request.form['max_random_value'])
       except Exception as e:
         raise RuntimeError('Please provide a number '
                            'for the max random value.')
-      flash('Matrix parameters: Rows = {}, Columns = {}, Max random value = {}'
+      flash('Matrix parameters: Rows = {}, '
+                               'Columns = {}, '
+                               'Max random value = {}, '
+                               'Number to be create = {}'
             .format(request.form['matrix_rows'],
                     request.form['matrix_columns'],
-                    request.form['max_random_value']))
+                    request.form['max_random_value'],
+                    request.form['number_matrices']))
       return render_template('index.html')
     except Exception as e:
       flash(e)
